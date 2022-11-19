8ccbc914b34dd77a7c28758f816c249b4a213e02
yu-ace <870941399@qq.com>
2022-11-19 10:40:07 +0800

    第118题

--- src/com/zyk/week3/LeetCode88.java
+++ src/com/zyk/week3/LeetCode88.java
@@ -10,25 +10,71 @@ package com.zyk.week3;
 public class LeetCode88 {
 
     public static void main(String[] args) {
-        int[] a = {0};
-        int m = 0;
-        int[] b = {1};
-        int n = 1;
+        int[] a = {1,2,3,0,0,0};
+        int m = 3;
+        int[] b = {2,5,6};
+        int n = 3;
         merge(a,m,b,n);
     }
 
+//    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+//        for(int i = m;i < nums1.length;i++){
+//            nums1[i] = nums2[i - m];
+//        }
+//        for(int i = 0;i < nums1.length;i++){
+//            for(int j = i;j < nums1.length;j++){
+//                if(nums1[i] > nums1[j]){
+//                    int tmp = nums1[i];
+//                    nums1[i] = nums1[j];
+//                    nums1[j] = tmp;
+//                }
+//            }
+//        }
+//    }
+
+    //双指针法
+//    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+//        int p1 = 0, p2 = 0;
+//        int[] sorted = new int[m + n];
+//        int cur;
+//        while (p1 < m || p2 < n) {
+//            if (p1 == m) {
+//                cur = nums2[p2];
+//                p2++;
+//            } else if (p2 == n) {
+//                cur = nums1[p1];
+//                p1++;
+//            } else if (nums1[p1] < nums2[p2]) {
+//                cur = nums1[p1];
+//                p1++;
+//            } else {
+//                cur = nums2[p2];
+//                p2++;
+//            }
+//            sorted[p1 + p2 - 1] = cur;
+//        }
+//        for (int i = 0; i != m + n; ++i) {
+//            nums1[i] = sorted[i];
+//        }
+//    }
+
+    //你双指针法
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        for(int i = m;i < nums1.length;i++){
-            nums1[i] = nums2[i - m];
-        }
-        for(int i = 0;i < nums1.length;i++){
-            for(int j = i;j < nums1.length;j++){
-                if(nums1[i] > nums1[j]){
-                    int tmp = nums1[i];
-                    nums1[i] = nums1[j];
-                    nums1[j] = tmp;
-                }
+        int p1 = m - 1, p2 = n - 1;
+        int tail = m + n - 1;
+        int cur;
+        while (p1 >= 0 || p2 >= 0) {
+            if (p1 == -1) {
+                cur = nums2[p2--];
+            } else if (p2 == -1) {
+                cur = nums1[p1--];
+            } else if (nums1[p1] > nums2[p2]) {
+                cur = nums1[p1--];
+            } else {
+                cur = nums2[p2--];
             }
+            nums1[tail--] = cur;
         }
     }
+
 }
---
+++ src/week4/LeetCode118.java
@@ -0,0 +1,31 @@
+package week4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一个非负整数 numRows，生成「杨辉三角」的前 numRows 行。
+ *
+ * 在「杨辉三角」中，每个数是它左上方和右上方的数的和。
+ *
+ * 需要了解用List创建二维数组的方法，以及注意取值需要用方法。
+ */
+public class LeetCode118 {
+
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> a = new ArrayList<List<Integer>>();
+        for(int i = 0;i < numRows;i++){
+            List<Integer> b = new ArrayList<>();
+            for(int j = 0;j <= i;j++){
+                if(j == 0 || i == j){
+                    b.add(1);
+                }else{
+                    b.add(a.get(i-1).get(j-1) + a.get(i-1).get(j));
+                }
+            }
+            a.add(b);
+        }
+        return a;
+    }
+
+}
