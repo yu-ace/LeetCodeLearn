131566116329cb2efa2497d07e5432349ea0a15b
yu-ace <870941399@qq.com>
2022-11-19 10:40:07 +0800

    第88题

--- src/com/zyk/week3/LeetCode83.java
+++ src/com/zyk/week3/LeetCode83.java
@@ -3,6 +3,7 @@ package com.zyk.week3;
 /**
  * 给定一个已排序的链表的头 head ， 删除所有重复的元素，使每个元素只出现一次 。返回 已排序的链表 。
  */
+
 public class LeetCode83 {
 
     public ListNode deleteDuplicates(ListNode head) {
---
+++ src/com/zyk/week3/LeetCode88.java
@@ -0,0 +1,34 @@
+package com.zyk.week3;
+
+/**
+ * 给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
+ *
+ * 请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
+ *
+ * 注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。为了应对这种情况，nums1 的初始长度为 m + n，其中前 m 个元素表示应合并的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。
+ */
+public class LeetCode88 {
+
+    public static void main(String[] args) {
+        int[] a = {0};
+        int m = 0;
+        int[] b = {1};
+        int n = 1;
+        merge(a,m,b,n);
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        for(int i = m;i < nums1.length;i++){
+            nums1[i] = nums2[i - m];
+        }
+        for(int i = 0;i < nums1.length;i++){
+            for(int j = i;j < nums1.length;j++){
+                if(nums1[i] > nums1[j]){
+                    int tmp = nums1[i];
+                    nums1[i] = nums1[j];
+                    nums1[j] = tmp;
+                }
+            }
+        }
+    }
+}
