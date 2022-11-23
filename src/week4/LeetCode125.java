b33ea89421ae3911d3ad6e30f926dd1891be98e9
yu-ace <870941399@qq.com>
2022-11-23 11:13:32 +0800

    第125题

---
+++ src/week4/LeetCode125.java
@@ -0,0 +1,35 @@
+package week4;
+
+/**
+ * 如果在将所有大写字符转换为小写字符、并移除所有非字母数字字符之后，短语正着读和反着读都一样。则可以认为该短语是一个 回文串 。
+ *
+ * 字母和数字都属于字母数字字符。
+ *
+ * 给你一个字符串 s，如果它是 回文串 ，返回 true ；否则，返回 false 。
+ */
+public class LeetCode125 {
+
+
+    public boolean isPalindrome(String s) {
+        int n = s.length();
+        int left = 0;
+        int right = n - 1;
+        while(left < right){
+            //从左开始判断，如果不是字母或数字，指针就像右移1
+            while(left < right && !Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }
+            while(left < right && !Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            }
+            if(left < right){
+                if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+                    return false;
+                }
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+}
