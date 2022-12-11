dec48fa04af118bff3e2fa4d199c43542655a9c9
yu-ace <870941399@qq.com>
2022-12-11 11:14:16 +0800

    88

---
+++ src/com/zyk/week4_1/LeetCode118.java
@@ -0,0 +1,29 @@
+package com.zyk.week4_1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一个非负整数 numRows，生成「杨辉三角」的前 numRows 行。
+ *
+ * 在「杨辉三角」中，每个数是它左上方和右上方的数的和。
+ */
+public class LeetCode118 {
+
+    public List<List<Integer>> generate(int numRows){
+        List<List<Integer>> a = new ArrayList<List<Integer>>();
+        for(int i = 0;i < numRows;i++){
+            List<Integer> b = new ArrayList<>();
+            for(int j = 0;j <= i;j++){
+                if(j == 0 || j == i){
+                    b.add(1);
+                }else{
+                    b.add(a.get(i-1).get(j-1) + a.get(i - 1).get(j));
+                }
+            }
+            a.add(b);
+        }
+        return a;
+    }
+
+}
