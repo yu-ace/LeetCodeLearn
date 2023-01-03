486c44d91c9b2a6da53ac48c72c96f166dd6fdc4
yu-ace <870941399@qq.com>
2023-01-03 20:51:03 +0800

    83

---
+++ src/com/zyk/week3_3/LeetCode83.java
@@ -0,0 +1,24 @@
+package com.zyk.week3_3;
+
+/**
+ * 给定一个已排序的链表的头 head ， 删除所有重复的元素，使每个元素只出现一次 。返回 已排序的链表 。
+ */
+public class LeetCode83 {
+
+
+    public ListNode deleteDuplicates(ListNode head){
+        if(head == null){
+            return head;
+        }
+        ListNode p = head;
+        while(p.next != null){
+            if(p.val == p.next.val){
+                p.next = p.next.next;
+            }else{
+                p = p.next;
+            }
+        }
+        return head;
+    }
+
+}
