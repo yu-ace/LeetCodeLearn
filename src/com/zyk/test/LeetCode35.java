bb05f560559ca992d6828ca8c3b74e8ac35b9283
yu-ace <870941399@qq.com>
2023-01-31 10:02:28 +0800

    35

---
+++ src/com/zyk/test/LeetCode35.java
@@ -0,0 +1,25 @@
+package com.zyk.test;
+
+/**
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ *
+ * 请必须使用时间复杂度为 O(log n) 的算法。
+ */
+public class LeetCode35 {
+
+    public int searchInsert(int[] nums, int target){
+        int n = nums.length;
+        int left = 0;
+        int right = n - 1;
+        while (right >= left){
+            int mid = (right - left) / 2 + left;
+            if(nums[mid] >= target){
+                n = mid;
+                right = mid - 1;
+            }else{
+                left = mid + 1;
+            }
+        }
+        return n;
+    }
+}
